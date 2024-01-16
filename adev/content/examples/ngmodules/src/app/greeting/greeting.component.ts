dfc96b312ad80c7433ea7a09a6353354e88ecd28
Joey Perrott <josephperrott@gmail.com>
2024-01-16 18:36:56 +0000

    refactor(docs-infra): move all content outside of the adev project source code
    Move the content files to a separate directory from the adev source code

---
+++ adev/content/examples/ngmodules/src/app/greeting/greeting.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { UserService } from './user.service';
+
+@Component({
+  selector: 'app-greeting',
+  templateUrl: './greeting.component.html',
+})
+export class GreetingComponent {
+  title = 'NgModules';
+  user = '';
+
+  constructor(userService: UserService) {
+    this.user = userService.userName;
+  }
+}
